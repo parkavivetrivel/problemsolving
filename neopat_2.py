6b847c91d719d8d7099acb0d71c393266dfa1d1a
parkavivetrivel <bhargavivetrivel27@gmail.com>
2024-04-07 18:54:27 +0530

    (special character)crt: aabbcc wrong:aabc.

---
+++ neopat_2.py
@@ -0,0 +1,22 @@
+global second
+a = "ikkkkkkkkkkbgh"
+global e
+global first
+b = 3
+c = 0
+d = []
+for i in range(0,len(a)):
+    first = i
+    d.append(a[first])
+    for j in range(i,len(a)):
+        if a[j] not in d:
+            d.append(a[j])
+            e = len(d)
+    if e == b:
+        print(a[first:len(a)])
+        break
+    else:
+        d.clear()
+
+
+
