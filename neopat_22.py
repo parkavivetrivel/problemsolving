501dc1c54a6551e3ddeae5cb4f1e148cf0f88057
parkavivetrivel <bhargavivetrivel27@gmail.com>
2024-05-19 18:59:12 +0530

    saddle point matrix(flag = True/False)

---
+++ neopat_22.py
@@ -0,0 +1,18 @@
+a = 3
+b = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+c = []
+#d = 0
+saddle = False
+for i in range(0, len(b)):
+    least_row_value = min(b[i])
+    tempIndex = b[i].index(min(b[i]))
+    d = 0
+    for j in range(0, len(b)):
+        if least_row_value >= b[j][tempIndex]:
+            d += 1
+    if d == len(b):
+        print(i, tempIndex,least_row_value)
+        saddle = True
+        break
+if not saddle:
+    print("no saddle point")
