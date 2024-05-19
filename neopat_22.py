d214e2395eebac5de88208d0bada706dd1a76057
parkavivetrivel <bhargavivetrivel27@gmail.com>
2024-05-19 19:02:58 +0530

    saddle point matrix(flag = True/False)

--- neopat_22.py
+++ neopat_22.py
@@ -1,7 +1,7 @@
 a = 3
 b = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 c = []
-#d = 0
+#d = 0 why wrong when it is declared globaly
 saddle = False
 for i in range(0, len(b)):
     least_row_value = min(b[i])
